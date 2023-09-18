3b9256b1a07831629cd4316301de7d60ad68116f
RedJyu <piotr.stezycki@protonmai.com>
2023-09-18 16:08:52 +0200

    finished

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,7 +1,18 @@
+import { toast } from 'react-toastify';
+
 function SingleColor({ color, index }) {
   const { hex, weight } = color;
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      await navigator.clipboard.writeText(`#${hex}`);
+      toast.success('Color Copied');
+    } else {
+      toast.error('clipboard not working');
+    }
+  };
   return (
     <article
+      onClick={saveToClipboard}
       className={index > 10 ? 'color color-light' : 'color'}
       style={{ background: `#${hex}` }}
     >
